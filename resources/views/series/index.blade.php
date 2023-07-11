f12914136291ddb6948664264e4be91fcb86aefe
Lucas Mendes Martins <martinslmendes@gmail.com>
2023-07-10 22:56:00 -0300

    feat: styled views

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,8 +1,8 @@
 <x-layout title="Séries">
-    <a href="/series/create">Adicionar</a>
-    <ul>
+    <a href="/series/create" class="btn btn-dark mb-2">Adicionar</a>
+    <ul class="list-group">
         @foreach ($series as $serie)
-            <li>{{ $serie }}</li>
+            <li class="list-group-item">{{ $serie }}</li>
         @endforeach
     </ul>
 </x-layout>
